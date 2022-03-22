1a9c8e8227ad8b72ccdf8493c4e7c2c592b91294
NatiG <nati.g5684@gmail.com>
2022-03-22 16:37:03 +0300

    Add comment.js module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import './style.css';
-
+import { sendComment, getComment } from './modules/comment.js';
---
+++ src/modules/comment.js
@@ -0,0 +1,34 @@
+const commentPOST = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/eI5R14OL1AS8fWw5Kmjy/comments';
+const commentGET = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/eI5R14OL1AS8fWw5Kmjy/comments?item_id=item1'
+
+// Send data to API
+const sendComment = async () => {
+    const settings = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify({
+            item_id: item1,
+            username: '',
+            comment: '',
+        }),
+    };
+
+    try {
+        const fetchRes = fetch(commentPOST, settings);
+        const resPOST = await fetchRes.json();
+        return resPOST;
+    } catch (error) {
+        return error;
+    }
+};
+
+// Get data from API
+const getComment = async () => {
+    const resGET = await fetch(commentGET);
+    const resJSON = await resGET.json();
+    return resJSON;
+};
+
+export { sendComment, getComment };
