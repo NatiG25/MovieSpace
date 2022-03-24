62f8ed176bd921ce442bb6fd28f94de1b76e8c44
NatiG <nati.g5684@gmail.com>
2022-03-24 12:04:49 +0300

    Add xMark

---
+++ src/modules/comment.js
@@ -0,0 +1,36 @@
+import { username, commentArea } from "./popupComments.js";
+
+const commentPOST = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/eI5R14OL1AS8fWw5Kmjy/comments';
+const commentGET = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/eI5R14OL1AS8fWw5Kmjy/comments?item_id=item1'
+
+// Send data to API
+const sendComment = async (itemId) => {
+    const settings = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify({
+            item_id: itemId,
+            username: username.value,
+            comment: commentArea.value,
+        }),
+    };
+
+    try {
+        const fetchRes = fetch(commentPOST, settings);
+        const resPOST = await fetchRes.json();
+        return resPOST;
+    } catch (error) {
+        return error;
+    }
+};
+
+// Get data from API
+const getComment = async () => {
+    const resGET = await fetch(commentGET);
+    const resJSON = await resGET.json();
+    return resJSON;
+};
+
+export { sendComment, getComment };
--- src/modules/popupComments.js
+++ src/modules/popupComments.js
@@ -1,5 +1,4 @@
-import { tvApiUrl } from "./apiCall.js";
-import { getComment, sendComment } from "./comment.js";
+import { sendComment } from "./comment.js";
 
 // const movieData = async (movieID) => {
 //   console.log(movieData());
@@ -27,7 +26,7 @@ const popupDisplay = async (data) => {
 
           popup.innerHTML += `
     <main class="popup-container">
-    <span class="xMark"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg></span>
+    <div class="xMark"><i id="closeBtn" class="fas fa-times" alt='#'></i>div>
       <div class="popup-headTitle">
         <img src="${item.image.medium}" />
         <h2>${item.name}</h2>
@@ -48,10 +47,10 @@ const popupDisplay = async (data) => {
       </section>
     </main>
   `;
-  submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    sendComment(item.id.toString());
-  });
+//   submitBtn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     sendComment(item.id.toString());
+//   });
           document.body.style.overflowY = "hidden";
           popup.style.display = "block";
           document.body.prepend(popup);
