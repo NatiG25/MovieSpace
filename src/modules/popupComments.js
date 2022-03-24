9e027fa7683fca48c8fa65c20c466752ce59e409
NatiG <nati.g5684@gmail.com>
2022-03-24 14:15:20 +0300

    Delete dist

--- src/modules/popupComments.js
+++ src/modules/popupComments.js
@@ -14,19 +14,21 @@ const username = document.querySelector(".user");
 const commentArea = document.querySelector(".comment");
 
 const popup = document.createElement("section");
+const submitBtn = document.querySelector('.submitBtn');
+
+
 
 const popupDisplay = async (data) => {
   document.body.addEventListener("click", (event) => {
     if (event.target.className === "commentBtn") {
       const commentId = event.target.parentNode.querySelector("button").id;
-      const submitBtn = document.querySelector('.submitBtn');
 
       data.forEach((item) => {
         if (item.id.toString() === commentId.toString()) {
 
           popup.innerHTML += `
     <main class="popup-container">
-    <div class="xMark"><i id="closeBtn" class="fas fa-times" alt='#'></i>div>
+    <span><i class="fas fa-times" alt='#'></i></span>
       <div class="popup-headTitle">
         <img src="${item.image.medium}" />
         <h2>${item.name}</h2>
@@ -47,20 +49,33 @@ const popupDisplay = async (data) => {
       </section>
     </main>
   `;
-//   submitBtn.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     sendComment(item.id.toString());
-//   });
+
+      
           document.body.style.overflowY = "hidden";
           popup.style.display = "block";
           document.body.prepend(popup);
+          removePopup();
         }
       });
     }
   });
 };
 
+const removePopup = () => {
+  document.addEventListener('click', (event) => {
+    if (event.target.className === 'fas') {
+      popup.style.display = 'none';
+      document.body.style.overflowY = ('auto');
+    }
+  });
+};
 
+// const submitComment = () => {
+//   submitBtn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     sendComment(item.id.toString());
+//   });
+// }
 
 
 export { popupDisplay, username, commentArea };
