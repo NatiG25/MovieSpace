659165127a960f263f31c82784e85fd2c93b06ba
Nicholas Mutale <nicomutale@gmail.com>
2022-03-25 13:53:52 +0200

    update logo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import './modules/logo.png';
+import './modules/asset/logo.svg';
 import getMovies from './modules/apiCall.js';
 import render from './modules/render.js';
 import popupDisplay from './modules/popupComments.js';
